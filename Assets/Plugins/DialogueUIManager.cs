a1ff772dd317390e32afdffbe34ef97cca0d5734
Lucas Fernandes <84829810+notluxca@users.noreply.github.com>
2024-08-27 16:19:27 -0300

    Update DialogueUIManager.cs

--- Assets/Plugins/DialogueUIManager.cs
+++ Assets/Plugins/DialogueUIManager.cs
@@ -5,58 +5,49 @@ using TMPro;
 using UnityEngine;
 using DG.Tweening;
 
-
 [RequireComponent(typeof(DialogueGroupSelector))]
 public class DialogueUIManager : MonoBehaviour
 {
     private bool dialogueHappening;
 
-    [Header("Dialogue UI Data")]
-    public CharacterDialogueAnimations characterterAnimations;
-    public GameObject dialogueUI;
-    public TMP_Text dialogText;
-    public TMP_Text characterNameText;
-    public CanvasGroup canvasGroup;
-
-    public Transform talkingPos;
-    public Transform listeningPos;
-
-    public Animator[] talkingCharacters; // Array que conterá os animators dos personagens
-
+    [Header("References")]
+    [SerializeField] CharacterDialogueAnimations characterterAnimations;
+    [SerializeField] GameObject dialogueUI;
+    [SerializeField] TMP_Text dialogText;
+    [SerializeField] TMP_Text characterNameText;
+    [SerializeField] CanvasGroup canvasGroup;
+    [SerializeField] Transform talkingPos, listeningPos;
     public GameObject charactersParent;
-
-    public Animator currentTalkingCharacter;
-    public Animator lastTalkingCharacter;
-
-    public float popDuration = 0.2f; // Duration of the pop animation
-    public float popScale = 1.2f; // The scale to pop to
-    private Vector3 originalScale;
-
     public DialogueGroupSelector dialogue;
     DSDialogueSO targetDialogue;
 
+    [Header("Dialogue Animation Settings")]
+    public float popDuration = 0.2f;
+    public float popScale = 1.2f;
+
+    // private data
+    private Animator[] talkingCharacters;
+    private Animator currentTalkingCharacter;
+    private Animator lastTalkingCharacter;
+    private Vector3 originalScale;
+
+
+
     private void Start()
     {
-        dialogueUI.SetActive(false); // inicializa sem a UI de diálogo ativa
+        dialogueUI.SetActive(false);
         dialogue = GetComponent<DialogueGroupSelector>();
-        targetDialogue = dialogue.targetDialogue;
 
-        // Preenche o array com os personagens
         SetUpCharacters();
     }
 
     void OnEnable()
     {
         targetDialogue = dialogue.targetDialogue;
-        dialogText.text = targetDialogue.RequestText;
-        characterNameText.text = targetDialogue.Actor.ToString();
-        Debug.Log(targetDialogue);
 
-
-        // Se for a primeira linha de diálogo, configura o personagem inicial
-        if (targetDialogue != null)
+        if (dialogueUI.activeSelf)
         {
-            SetCharacterForDialogue(targetDialogue.Actor.ToString(), targetDialogue.speechAnimation);
+            InitializeDialogueUI();
         }
     }
 
@@ -87,44 +78,33 @@ public class DialogueUIManager : MonoBehaviour
 
     void UpdateDialogue()
     {
-
         string newActor = targetDialogue.Actor.ToString();
         PlayPopAnimation();
-        // Salva o personagem atual antes de mudar para o próximo
+
         lastTalkingCharacter = currentTalkingCharacter;
-        // Debug.Log($"last talking char {lastTalkingCharacter.gameObject.name} and new is {newActor}");
 
         targetDialogue = targetDialogue.Choices[0].NextDialogue;
         dialogText.text = targetDialogue.RequestText;
         characterNameText.text = targetDialogue.Actor.ToString();
 
-        Debug.Log($"{lastTalkingCharacter.gameObject.name} and {targetDialogue.Actor}");
-        // Se o novo diálogo for de outro personagem, atualiza as posições e animações
         if (lastTalkingCharacter.gameObject.name != targetDialogue.Actor.ToString())
         {
-            Debug.Log("teste de mudar persoinagem");
             SetCharacterForListening(lastTalkingCharacter);
         }
 
         SetCharacterForDialogue(newActor, targetDialogue.speechAnimation);
     }
 
-
     private void PlayPopAnimation()
     {
-        // Reset to original scale before playing the animation
         originalScale = new Vector3(1.4f, 1.4f, 1);
 
-        // Animate the scale to create the pop effect
         characterNameText.transform.DOScale(popScale, popDuration / 2).SetEase(Ease.OutQuad)
             .OnComplete(() =>
             {
-                // Scale back to the original size after the pop
                 characterNameText.transform.DOScale(originalScale, popDuration / 2).SetEase(Ease.InQuad);
             });
-
     }
-
 
     void SetCharacterForDialogue(string actorName, string speechAnimation)
     {
@@ -134,7 +114,6 @@ public class DialogueUIManager : MonoBehaviour
             currentTalkingCharacter.gameObject.SetActive(true);
             currentTalkingCharacter.transform.position = talkingPos.position;
             currentTalkingCharacter.Play(speechAnimation, 0);
-            // Debug.Log("primeira iteração feita");
         }
     }
 
@@ -151,31 +130,38 @@ public class DialogueUIManager : MonoBehaviour
     {
         dialogueUI.SetActive(true);
         dialogueHappening = true;
-        currentTalkingCharacter = currentTalkingCharacter = FindCharacterAnimatorByName(targetDialogue.speechAnimation);
         targetDialogue = dialogue.targetDialogue;
+
+        InitializeDialogueUI();
+    }
+
+    void InitializeDialogueUI()
+    {
         dialogText.text = targetDialogue.RequestText;
         characterNameText.text = targetDialogue.Actor.ToString();
-        // Se for a primeira linha de diálogo, configura o personagem inicial
+
+        PlayPopAnimation();
+
         if (targetDialogue != null)
         {
             SetCharacterForDialogue(targetDialogue.Actor.ToString(), targetDialogue.speechAnimation);
         }
-
     }
 
     void FinishDialogue()
     {
         dialogueUI.SetActive(false);
         dialogueHappening = false;
+        foreach (Animator obj in talkingCharacters)
+        {
+            obj.gameObject.SetActive(false);
+        }
     }
 
     void SetUpCharacters()
     {
-
-
         if (charactersParent != null)
         {
-            // Obtém todos os animators dos filhos do GameObject "Characters"
             talkingCharacters = charactersParent.GetComponentsInChildren<Animator>(true);
         }
         else
